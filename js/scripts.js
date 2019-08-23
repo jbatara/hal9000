956c0503b1e54efc6a619b747ce22da2dc10c23d
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-08-23 16:04:30 -0700

    add feature ask for name to replace name in welcome messages

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,48 @@
 $(document).ready(function() {
-  $(".hello").show();
-  setTimeout(function(){
-    $(".hello").removeClass("cursor");
-    $(".question").addClass("cursor");
-    $(".question").show();
-    setTimeout(function(){
-      $("form").show();
+  setTimeout(function() {
+    $(".userName").show();
+  }, 2000);
+
+  $(".userName").submit(function() {
+    event.preventDefault();
+    var userName = $(".userName input").val();
+    $(".userName").hide();
+    $(".hello").append(userName + ".");
+    $(".hello").addClass("cursor");
+    setTimeout(function() {
+      removeCursor($(".hello"));
+      $(".question").addClass("cursor");
+      $(".question").append(userName + "?");
+      $(".question").show();
+      setTimeout(function() {
+        $(".number").show();
+      }, 4000);
     }, 3000);
-  }, 3000);
+  });
 
 
-  $("form").submit(function(event) {
+  $("input.number").click(function(event) {
+    removeCursor($(".question"));
+  });
+
+  $("form.number").submit(function(event) {
 
     event.preventDefault();
     var userInput = $("[name=number]").val();
     var answer = numberQuery(userInput);
-    answer.forEach(function(number,i){
-        $(".answer").append("<p>" + number + "</p>");
+    $(".hello, .question, .number").hide();
+    answer.forEach(function(number, i) {
+      $(".answer").append("<p>" + number + "</p>");
     });
     $(".answer").show();
-    $(".question").hide();
   });
 });
 
 
-function allNumbers(number){
+function allNumbers(number) {
   var answer = [];
   number = parseInt(number);
-  for(var i=0; i<=number; i++){
+  for (var i = 0; i <= number; i++) {
     answer.push(String(i));
   }
   return answer;
@@ -38,7 +53,7 @@ function numberQuery(number) {
   var answer = [];
   var userNumbers = allNumbers(number);
 
-  for (var i=0; i < userNumbers.length; i++) {
+  for (var i = 0; i < userNumbers.length; i++) {
     if (userNumbers[i].includes("3")) {
       answer.push(i + ": I'm sorry, Dave. I'm afraid I can't do that.");
     } else if (userNumbers[i].includes("2")) {
@@ -50,4 +65,9 @@ function numberQuery(number) {
     }
   }
   return answer;
+}
+
+function removeCursor(element) {
+  element.removeClass("cursor");
+  element.css("display", "block");
 }
