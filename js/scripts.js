1176f9e5bfabc6fa3319e1043c240e36d1f91844
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-08-23 09:39:54 -0700

    add working functionaly for numberQuery and print answer to webpage

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+
+
+  $("form").submit(function(event) {
+
+    event.preventDefault();
+    var userInput = $("[name=number]").val();
+    var answer = numberQuery(userInput);
+    answer.forEach(function(number){
+    $(".answer").append("<p>" + number + "</p>");
+    });
+  });
+});
+
+
+function allNumbers(number){
+  var answer = [];
+  number = parseInt(number);
+  for(var i=0; i<=number; i++){
+    answer.push(String(i));
+  }
+  return answer;
+}
+
+
+function numberQuery(number) {
+  var answer = [];
+  var userNumbers = allNumbers(number);
+
+  for (var i=0; i < userNumbers.length; i++) {
+    if (userNumbers[i].includes("3")) {
+      answer.push(i + ": I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if (userNumbers[i].includes("2")) {
+      answer.push(i + ": Boop!");
+    } else if (userNumbers[i].includes("1")) {
+      answer.push(i + ": Beep!");
+    } else {
+      answer.push(userNumbers[i]);
+    }
+  }
+  return answer;
+}
